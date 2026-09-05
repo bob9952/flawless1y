d4366a53f3467c08cdae30035cda416da74d858f
bob9952 <thegreatsubotic@gmail.com>
2026-09-05 20:11:54 +0200

    azuriran broj telefona

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,6 +1,6 @@
 export const contact = {
-  phoneDisplay: "+381 60 193 71 77",
-  phoneHref: "+381601937177",
+  phoneDisplay: "+381 61 177 42 45",
+  phoneHref: "+381611774245",
   instagram: "@flawless1y.rs",
   instagramHref: "https://www.instagram.com/flawless1y.rs",
   email: "goran-dubinsko@flawless1y.rs",
