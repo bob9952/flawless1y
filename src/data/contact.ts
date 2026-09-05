dd0ac272d7a4f3551fc96a9d2f1600a649a2b041
bob9952 <thegreatsubotic@gmail.com>
2026-09-05 16:07:10 +0200

    ig link

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -2,7 +2,7 @@ export const contact = {
   phoneDisplay: "+381 60 193 71 77",
   phoneHref: "+381601937177",
   instagram: "@flawless1y.rs",
-  instagramHref: "https://www.instagram.com/flawless1y.rs?igsi=MWZhOGd2cm50MDJhcQ==",
+  instagramHref: "https://www.instagram.com/flawless1y.rs",
   email: "goran-dubinsko@flawless1y.rs",
   areaServed: "Beograd",
 }
